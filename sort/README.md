ec91151b5d99c465be18e2192cc0523636d0f5f1
chihyanghsu0805 <chihyanghsu0805@gmail.com>
2021-08-29 22:40:22 -0400

    add selection sort

--- sort/README.md
+++ sort/README.md
@@ -1,4 +1,7 @@
 # Sorting Algorithms
 
 ## Bubble Sort
-Bubble sort compares each pair and swap smaller one to frony, O(N<sup>2</sup>).
+Bubble sort compares each pair and swap smaller one to front, O(N<sup>2</sup>).
+
+## Selection Sort
+Selection finds minimum and swap to corresponding index, O(N<sup>2</sup>).
--- sort/test_sort.py
+++ sort/test_sort.py
@@ -1,6 +1,7 @@
 """Run Tests."""
 
 from bubble_sort import bubble_sort
+from selection_sort import selection_sort
 
 ARR = [5, 4, 3, 2, 1]
 
@@ -8,3 +9,8 @@ ARR = [5, 4, 3, 2, 1]
 def test_bubble_sort():
     """Test Bubble Sort."""
     assert bubble_sort(ARR) == sorted(ARR)
+
+
+def test_selection_sort():
+    """Test Selection Sort."""
+    assert selection_sort(ARR) == sorted(ARR)
